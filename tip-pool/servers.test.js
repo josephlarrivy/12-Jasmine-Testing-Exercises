c168ea22ed7a14155008b0d539ae5d67ff5dc09f
josephlarrivy <joseph.larrivy@gmail.com>
2022-08-22 15:00:53 -0500

    servers.test.js updated to run tests

--- tip-pool/servers.test.js
+++ tip-pool/servers.test.js
@@ -6,12 +6,29 @@ describe("Servers test (with setup and tear-down)", function() {
 
   it('should add a new server to allServers on submitServerInfo()', function () {
     submitServerInfo();
-
     expect(Object.keys(allServers).length).toEqual(1);
     expect(allServers['server' + serverId].serverName).toEqual('Alice');
   });
 
+  it('should not add new server if submitServerInfo() is blank', function() {
+    //sets server info to blank
+    serverNameInput.value = '';
+    //submits server info
+    submitServerInfo();
+    //says what to expect
+    expect(Object.kays(allServers).length).toEqual(0);
+  });
+
+  it('should update server table', function() {
+    expect(curTdList.length).toEqual (3);
+    expect(curTdList[0].innerText).toEqual ('Alice');
+    expect(curTdList[1].innerText).toEqual ('X');
+  });
+
+
   afterEach(function() {
-    // teardown logic
+    serverId=0;
+    serverTbody.innerHTML = '';
+    allServers = {};
   });
 });
