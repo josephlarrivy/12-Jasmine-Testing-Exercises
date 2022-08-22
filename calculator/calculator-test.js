0cf62700e1008462221341fdcdd33a8a969e9093
josephlarrivy <joseph.larrivy@gmail.com>
2022-08-22 10:02:40 -0500

    loan calculator tests working

--- calculator/calculator-test.js
+++ calculator/calculator-test.js
@@ -1,11 +1,11 @@
 
 it('should calculate the monthly rate correctly', function () {
-  // ...
+  const values = {amount:15000, years:8, rate:4};
+  expect(calculateMonthlyPayment(values)).toEqual('182.84');
 });
 
 
 it("should return a result with 2 decimal places", function() {
-  // ..
+  const values = {amount:12345, years:5.234, rate:3.234};
+  expect(calculateMonthlyPayment(values)).toEqual('216.48');
 });
-
-/// etc
